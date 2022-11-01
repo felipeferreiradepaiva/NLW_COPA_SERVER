17952cd73a10d02672120a15b02946a6a463645c
Felipe Paiva <ffpaiva@outlook.com>
2022-11-01 14:59:35 -0300

    chore: add react, prisma and typescript

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import Fastify from "fastify";
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+    log: ['query'],
+})
+
+async function bootstrap(){ 
+    const fastify = Fastify({
+        logger: true,
+    })
+
+    await fastify.register(cors, {
+        origin: true,
+    })
+    
+    // http://localhost:3333/pools/count -> 
+    fastify.get('/pools/count', async () => {
+        const count = await prisma.pool.count()
+
+        return { count }
+    })
+
+    await fastify.listen({ port: 3333, host: '0.0.0.0' })    
+}
+
+bootstrap()
